39cee6efec0cfdd0c8eed48a4f6f92237713d26e
Shubham Jain <shubhamjain5396.tis@gmail.com>
2024-07-05 10:33:09 +0530

    Updated Schedule

--- app/components/downloadbutton.tsx
+++ app/components/downloadbutton.tsx
@@ -1,33 +1,33 @@
-import React from 'react'
-import { IoMdDownload } from "react-icons/io";
+"use client"
+import React from 'react';
+import { IoMdDownload } from 'react-icons/io';
+import schedulepdf from "@/public/schedule.pdf"
 
 interface MyComponentProps {
   downloadbutton: string;
 }
 
+const scheduleurl = '../schedule.pdf';
+
 const MyComponent: React.FC<MyComponentProps> = ({ downloadbutton }) => {
+  const downloadFileAtURL = () => {
+    const fileName = 'Schedule.pdf';
+    const aTag = document.createElement('a');
+    aTag.href = scheduleurl;
+    aTag.setAttribute('download', fileName);
+    document.body.appendChild(aTag);
+    aTag.click();
+    aTag.remove();
+  };
+
   return (
-    <div>
-    {/* Download Schedule */}
-    <button><a 
-    className="flex place-content-center items-center text-white px-4 py-2 bg-footerpink rounded-full gap-1"
-    href="/Schedule.pdf" download>
-      Download Schedule{" "}<IoMdDownload className='place-items-center'/>
-    </a>
-    </button>
+    <div className='App'>
+      {/* Download Schedule */}
+      <button onClick={downloadFileAtURL} className="flex place-content-center items-center text-white px-4 py-2 bg-footerpink rounded-full gap-1">
+        Download Schedule <IoMdDownload className='place-items-center'/>
+      </button>
     </div>
   );
 };
 
 export default MyComponent;
-
-
-
-// Paste this code
-{/* 
-import MyComponent from "../components/downloadbutton"
-
-<div className="flex place-content-center rounded-full">
-<MyComponent downloadbutton='download' />
-</div>
- */}
