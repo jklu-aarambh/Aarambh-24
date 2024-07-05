61be9f88091f1251ebdee17df3861d8d192a8489
Shubham Jain <shubhamjain5396.tis@gmail.com>
2024-07-05 15:28:48 +0530

    Meta update

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const montserrat  = Montserrat ({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Aarambh",
   description:
-    "Come be a part of HackJKLU v3.0, the hackathon organized by the Council of Technical Affairs of JK Lakshmipat University, Jaipur. Join us from 15th to 17th March 2024 for a thrilling hackathon in Web2, Web3, IoT, Game dev/ AR/ VR, Cybersecurity and AI/ML. Register now!",
+    "Come be a part of Aarambh'24, the orientation programme organized by the Student Affairs of JK Lakshmipat University, Jaipur. Join us from 8th to 25th July 2024 for a thrilling orientation!",
   keywords:
     "Aarambh, Aarambh'24, Aarambh 2024, hackjklu, Aarambh JKLU 2024, orientation, orientation JKLU, orientation JK Lakshmipat University, orientation JKLU Jaipur, orientation JKLU Rajasthan, orientation JKLU India, JK Lakshmipat University, Jaipur, Rajasthan, India, hackathon, Jaipur hackathon, hackathon in march, hackathon in 2024, admission in JKLU, admission in JK Lakshmipat University, admission in JKLU Jaipur, admission in JKLU Rajasthan, admission in JKLU India, bachelor of design best college, bacheor of technology best college, best college jaipur",
 };
