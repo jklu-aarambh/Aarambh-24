44574b9e724e0ca8f1d4d3634d590f7bf09152a9
ShubhamJain-23 <shubhamjain5396.tis@gmail.com>
2024-07-06 19:04:01 +0530

    Updated speakers page

--- app/speakers/page.tsx
+++ app/speakers/page.tsx
@@ -41,45 +41,39 @@ const Team: React.FC = () => {
             <span
               className="text-black bg-clip-text"
             >
-              SPEAKERS LIST
+              SPEAKERS / FACILITATORS
             </span>
           </h3>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-[1rem] mx-[1rem] md:mx-[1rem]">
             {importedDataSpeakers.map((member) => (
               <div key={member.name} className="relative text-white">
-                <div className="absolute inset-0.5 md:-inset-0.5"></div>
-                <div className="relative flex flex-row md:flex-col align-center items-center justify-center p-4 md:p-2">
-                  <div className="w-1/2 md:mb-4 relative h-[8rem] md:h-[14rem] md:w-[14rem] overflow-hidden rounded-full shadow-lg md:mx-auto">
-                    <Image
-                      src={member.photo}
-                      alt={member.name}
-                      fill
-                      style={{ objectFit: "cover" }}
-                      className="rounded-full"
-                    />
-                  </div>
-                  <div className="w-1/2 md:w-full m-1">
-                    <div className="flex flex-col justify-center items-center">
-                      <div className="flex flex-row items-center">
-
-                        <ul className="flex sm:flex-row justify-center place-items-center lg:justify-between md:justify-center my-2">
-                          <li className="items-center lg:mx-4 mb-1">
-                            <Link href={member.linkedin}>
-                              <p className="font-bold text-black text-center hover:underline text-base md:text-xl mb-">
-                                {member.name}
-                              </p>
-                          </Link>
-                          </li>
-
-                        </ul>
-                      </div>
-                      <p className="flex justify-center text-center text-[#737373]">
-                        {member.event}
-                      </p>
-                    </div>
+              <div className="absolute inset-0.5 md:-inset-0.5"></div>
+              <div className="relative flex flex-row md:flex-col align-center items-center justify-center p-4 md:p-2">
+                <div className="w-1/2 md:mb-4 relative h-[8rem] md:h-[14rem] md:w-[14rem] overflow-hidden rounded-full shadow-lg md:mx-auto">
+                  <Image
+                    src={member.photo}
+                    alt={member.name}
+                    fill
+                    style={{ objectFit: "cover" }}
+                    className=""
+                  />
+                </div>
+                <div className="flex-grow"></div>
+                <div className="w-1/2 md:w-full m-2">
+                  <div className="flex flex-col justify-center items-center">
+                    <p className="font-bold text-black text-center text-lg md:text-xl mb-2">
+                      {member.name}
+                    </p>
+                    <Link href={member.linkedin}>
+                    <FaLinkedin className="h-6 w-6 md:h-8 md:w-8 bg-black  mr-2 p-1 rounded-lg" />
+                    </Link>                      
+                    <p className="flex justify-center text-center text-black md:pt-2">
+                      {member.event}
+                    </p>
                   </div>
                 </div>
               </div>
+            </div>
             ))}
           </div>
         </section>
